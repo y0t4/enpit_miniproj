a78252d2267e1045fc002e60fa35594b138b753e
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-09-06 13:25:09 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EnpitMiniproj::Application.config.secret_key_base = '367d4169893864592baa903df7945500162cbe24b1e939c2d6b5f60159e06040eb11d788c429a84b5a49030999d6adb3b680adc00e53d1d2620cbf45be60e05a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EnpitMiniproj::Application.config.session_store :cookie_store, key: '_enpit_miniproj_session'
