655df42c78e22a38bdbb98db50559025dabebec7
Yosuke OTA <yota@ns.ie.u-ryukyu.ac.jp>
2013-09-06 17:38:59 +0900

    indented

--- app/controllers/calculate_controller.rb
+++ app/controllers/calculate_controller.rb
@@ -1,18 +1,18 @@
 class CalculateController < ApplicationController
   def calc
-     a = params['a'].to_i
-     b = params['b'].to_i
-     func = params['func']
+    a = params['a'].to_i
+    b = params['b'].to_i
+    func = params['func']
 
-     if func == '+' then 
-     	@c = a+b
-     elsif func == '-' then 
-     	@c = a-b
-     elsif func == '*' then 
-     	@c = a*b
-     else
-     	@c = a/b
-     end
-     render 'calculate/index'
+    if func == '+' then
+      @c = a+b
+    elsif func == '-' then
+      @c = a-b
+    elsif func == '*' then
+      @c = a*b
+    else
+      @c = a/b
+    end
+    render 'calculate/index'
   end
 end
